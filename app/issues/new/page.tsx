44a3cef8031b5bcda67dca17f088429617af88df
armacomiguel <armacomiguel@gmail.com>
2024-06-28 10:36:53 -0700

    Añadiendo form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,16 +2,35 @@
 import React from 'react';
 import { TextField, Button } from '@radix-ui/themes'; 
 import SimpleMDE from "react-simplemde-editor";
+import {useForm, Controller} from 'react-hook-form';
+import axios from 'axios';
 import "easymde/dist/easymde.min.css";
+import {useRouter} from 'next/navigation';
+
+interface IssueForm {
+    title: string;
+    description: string;
+};
 
 const NewIssuePage = () => {
+
+    const router = useRouter();
+    const {register, control, handleSubmit} = useForm<IssueForm>();
+
   return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root placeholder="Titulo">
+    <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async(data) => {
+        await axios.post('/api/issues',data);
+        router.push('/issues');
+    })}>
+        <TextField.Root placeholder="Titulo" {...register('title')}>
         </TextField.Root>
-        <SimpleMDE placeholder="Descripción" />
+        <Controller 
+            name="description"
+            control={control}
+            render={({field}) => <SimpleMDE placeholder="Descripción" {...field} />}
+        />
         <Button>Crear Nuevo Detalle</Button>
-    </div>
+    </form>
   )
 }
 
